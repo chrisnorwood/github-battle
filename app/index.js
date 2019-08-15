fea973226541240f9f9a198804d6ddc6abf9af5f
Chris Norwood <cnorwood92@gmail.com>
2019-08-15 14:15:38 -0700

    Navbar holds navigation state

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ import Popular from './components/Popular'
 class App extends React.Component {
   render() {
     return (
-      <div class='container'>
+      <div className='container'>
         <Popular />
       </div>
     )
